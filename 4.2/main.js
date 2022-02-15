408bdff1010ebe35f9802645976fc9472b896d6c
fdenne <fernwerm.art@gmail.com>
2022-02-15 17:19:35 -0600

    4.2

---
+++ 4.2/main.js
@@ -0,0 +1,34 @@
+let rectX = 0;
+const rectHeight = 75;
+let rectY;
+let rectWidth = 75;
+let clickCount = 0;
+let speed;
+function setup () {
+    createCanvas(500, 500);
+    rectY = random(height - rectHeight);
+    speed = random(1, 3)
+}
+
+function draw () {
+    background("lightblue");
+    drawShape();
+    rectX += speed;
+    if(rectX > width) {
+        noLoop();
+        text("Your score was " + clickCount + "! Congratulations!", 100, 300);
+    }
+}
+
+function mousePressed() {
+    if((mouseX >= rectX && mouseX <= rectX + rectWidth) && (mouseY >= rectY && mouseY <= rectY + rectHeight)) {
+        clickCount++;
+        console.log("hit", clickCount);
+    }
+}
+
+function drawShape() {
+    fill("blue");
+    rect(rectX, rectY, rectWidth, rectHeight);
+
+}
