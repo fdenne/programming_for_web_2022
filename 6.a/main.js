02d28b6e87c38d5249fb5fd0511dbeecb68f45c1
fdenne <fernwerm.art@gmail.com>
2022-02-27 15:16:04 -0600

    6.a

---
+++ 6.a/main.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: "#myApp",
+    data: {
+        message: "I bet you didn't know all these facts about Llamas!",
+        llamaHeight: "Llamas can grow as much as 6 feet tall",
+        llamaPopulation: "there are around 160,000 llamas in the United States and Canada",
+        llamaWeight: "Llamas weigh between 280 and 450 pounds",
+        llamaCapacity: "Llamas can carry 25 to 30 percent of their body weight",
+        llamaFamily: "Llamas are pretty closely related to camels",
+        llamaAge: "about 3 million years ago, llamas' ancestors migrated to South America",
+        llamaNice: "Llamas don't bite",
+        llamaDiet: "Llamas are vegetarians and have very efficient digestive systems",
+        llamaBaby: "a baby llama is called a cria, which is Spanish for baby",
+        llamaAlpaca: "Llamas are generally about twice the size of alpacas"
+    }
+  });
+  
