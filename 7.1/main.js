c9b21e293cbade3f7e44ccc0bd64ccf313be2740
fdenne <fernwerm.art@gmail.com>
2022-03-04 20:28:32 -0600

    7.1

---
+++ 7.1/main.js
@@ -0,0 +1,10 @@
+Vue.createApp({
+    data () {
+        return {
+            llamaLink: "https://en.wikipedia.org/wiki/Llama",
+            llamaText: "scary llamas",
+            alpacaLink: "https://en.wikipedia.org/wiki/Alpaca",
+            alpacaText: "cuddly alpacas"
+        }
+    }
+}).mount("#myApp");
