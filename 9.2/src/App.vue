177e4676e4b9a56d5fd98b3584e9f67a08f7aceb
fdenne <fernwerm.art@gmail.com>
2022-03-28 14:10:17 -0500

    9.2

---
+++ 9.2/src/App.vue
@@ -0,0 +1,59 @@
+<script setup>
+import {reactive} from "vue";
+// javascript here
+import CamelidRow from "./components/CamelidRow.vue";
+const isWorking = true;
+const camelids = [
+      {
+        commonName: "Lama",
+        binomialName: "Lama glama",
+        order: "Artiodactyla",
+        family: "Camelidae",
+        genus: "Lama",
+        species: "L. glama",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Llama_lying_down.jpg/220px-Llama_lying_down.jpg",
+        id: 0
+        },
+      {
+        commonName: "Alpaca",
+        binomialName: "Vicugna pacos",
+        order: "Artiodactyla",
+        family: "Camelidae",
+        genus: "Vicugna",
+        species: "V. pacos",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Alpaca_%2831562329701%29.jpg/440px-Alpaca_%2831562329701%29.jpg",
+        id: 1
+      },
+    ];
+    const state = reactive({camelids})
+    function handleRowDelete(camelid) {
+      state.camelids = state.camelids.filter(item => {
+        return item !== camelid;
+      });
+    }
+</script>
+
+<template>
+  <!-- html goes here-->
+<div class="container">
+  <h1>Lama Alpaca</h1>
+  <CamelidRow 
+    v-for="camelid in state.camelids"
+    v-bind:key="camelid.id"
+    v-bind:camelid="camelid"
+    v-on:delete-row="handleRowDelete" />
+  <div>
+    
+  </div>
+</div>
+
+
+</template>
+
+<style>
+/* style-go-here */
+.container {
+  width: 600px;
+  margin: 20px auto 0;
+  }
+</style>
---
+++ 9.2/src/components/CamelidRow.vue
@@ -0,0 +1,41 @@
+<script setup>
+const props = defineProps(["camelid"]);
+console.log(props.camelid);
+// const triggerConsole = (camelid) => {
+   // console.log("you clicked", camelid.commonName);
+// }
+
+</script>
+
+<template>
+    <div class="camelid-row" v-on:click="triggerConsole(props.camelid)">
+    <h5>{{props.camelid.commonName}}</h5>
+    <img v-bind:src="props.camelid.image" v-bind:alt="props.camelid.commonName" v-on:click="$emit('imageClick', props.camelid.commonName)">
+    <button type="button" v-on:click="$emit('deleteRow', props.camelid)">Delete</button>
+    
+  </div>
+</template>
+
+<style scoped>
+
+ h5 {
+  text-align: center;
+  font-size: 18px;
+}
+ p {
+  font-size: 18px;
+  text-align: center;
+}
+.camelid-row {
+    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    border: 1px solid black;
+    margin: 10px;
+    border-radius: 5px;
+}
+ .camelid-row img {
+  max-width: 300px;
+  display: block;
+  margin: 5px auto 0;
+
+}
+</style>
